78270ecd487b2abeaa485e0ac5fdce3853ea781e
“test1 <lxjuzit@163.com>
2020-12-08 15:34:31 +0800

    更新1

---
+++ src/main/java/com/lanxaing/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.lanxaing.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+		System.out.println("更新1");
+
+		
+	}
+
+}
